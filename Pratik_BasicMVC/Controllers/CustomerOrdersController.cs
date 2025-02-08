4d76647b7b9341b7b012adc291f0e61ccc424fac
bburakbbasol <bburakbbasol@gmail.com>
2025-02-08 21:17:08 +0300

    Add project files.

---
+++ Pratik_BasicMVC/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pratik_BasicMVC.Models;
+
+namespace Pratik_BasicMVC.Controllers
+{
+	public class CustomerOrdersController : Controller
+	{
+		public IActionResult Index()
+
+		{
+			var customer = new Customer
+			{
+				Id = 1,
+				FirstName = "Burak",
+				LastName = "Başol",
+				Email="bburakbbasol@gmail.com",
+			
+			};
+			var orders = new List<Order>
+	{
+		new Order { Id = 1, ProductName = "Akhisar Köfte", Price = 280, Quantity = 10 },
+		new Order { Id = 2, ProductName = "Ali Nazik", Price = 300, Quantity = 4 },
+		new Order { Id = 3, ProductName = "Kelle Paça Çorbası", Price = 200, Quantity = 5 }
+	};
+
+			var viewModel = new CustomerOrderViewModel
+			{
+				Customer = customer,
+				Orders = orders.ToList()
+			};
+
+
+			return View(viewModel);
+		}
+	}
+}
---
+++ Pratik_BasicMVC/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace Pratik_BasicMVC.Models
+{
+	public class Order
+	{
+		public int Id { get; set; }
+		public string ProductName {  get; set; }
+
+		public decimal Price { get; set; }
+
+		public int Quantity {  get; set; }
+
+	}
+}
